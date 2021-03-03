8b2b64522bc1e97e2ced8026edc0fd077034339b
a20l <aidaytyn@gmail.com>
2021-03-03 10:34:25 -0600

    improved the converter by adding random number generator that takes num from (-100)  to (+100)

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 var cur_temp_c = 40;
-var cur_temp_f = 100;
-
-var in_f = (cur_temp_c * 9);
-in_f = in_f / 5;
-in_f = in_f + 32;
+var in_f = (cur_temp_c * 9)/5+32;
 
 
-var in_c = (cur_temp_f - 32);
-in_c = in_c * 5;
-in_c = in_c / 9;  
+// converting temp in F to C for the range -100 to 100
+const min=-100;
+const max=100;
+const cur_temp_f = Math.floor(Math.random()*(max-min+1))+min;
+var in_c = (cur_temp_f - 32)*5/9;
 
-document.write(cur_temp_c + " in Celsius will be " + in_f + '\u00B0' + 'F. ' );
+
+document.write(cur_temp_c + " \u00B0C will be " + in_f + '\u00B0F. ' );
 document.write("<br>"); 
-document.write(cur_temp_f + '\u00B0' +" in Fahrenheit will be " + in_c.toFixed(1) + '\u00B0' + 'C.' );
+document.write('It is ' + cur_temp_f + '\u00B0F today. That is ' + in_c.toFixed(1)  + '\u00B0C.');
+
